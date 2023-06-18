01fdcac06699fdbdddca38e963ac482c9f76e0c5
mac <mac@Yanir-MacBook.local>
2023-06-18 15:59:22 +0300

    server stracture

---
+++ server/server.ts
@@ -0,0 +1,31 @@
+import fastify from 'fastify';
+import cors from 'fastify-cors';
+import routes from './routes/collectionRoutes';
+
+const PORT = process.env.PORT || 3001;
+
+const app = fastify({
+  logger: true,
+});
+
+app.register(cors, {
+  origin: true,
+});
+
+routes.forEach((route) => {
+  app.route(route);
+});
+``
+const start = async () => {
+  try {
+    await app.listen(PORT);
+    const address = app.server.address();
+    const port = typeof address === 'string' ? address : address?.port;
+    app.log.info(`server listening on ${port}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
